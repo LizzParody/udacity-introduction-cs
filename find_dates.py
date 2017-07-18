bbdd7503a4513b98352d6d125c2b865513f8b537
Liz Parody <lizparody@gmail.com>
2017-07-18 16:23:37 -0400

    finished the exercises of finding days between dates

--- find_dates.py
+++ find_dates.py
@@ -1,5 +1,27 @@
+def isLeapYear(year):
+    if(year % 4 != 0):
+        return False
+    elif(year % 100 != 0):
+        return True
+    elif(year % 400 != 0):
+        return False
+    else:
+        return True
+
+def daysInMonth(year, month):
+    if(month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12):
+        return 31
+    elif(month == 2):
+        if(isLeapYear(year)):
+            return 29
+        else:
+            return 28
+    else:
+        return 30
+    return 30
+
 def nextDay(year, month, day):
-    if day < 30:
+    if day < daysInMonth(year, month):
         return year, month, day + 1
     elif month == 12:
         return year + 1, 1, 1
@@ -9,24 +31,43 @@ def nextDay(year, month, day):
 def dateIsBefore(year1, month1, day1, year2, month2, day2):
     if(year1 < year2):
         return True
-    elif(year1 == year2):
+    if(year1 == year2):
         if(month1 < month2):
             return True
-        elif(month1 == month2):
+        if(month1 == month2):
             return day1 < day2
     return False
 
 def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    assert not dateIsBefore(year2, month2, day2, year1, month1, day1)
     days = 0
-    while(dateIsBefore(year1, month1, day1, year2, month2, day2)):
+    while dateIsBefore(year1, month1, day1, year2, month2, day2):
         year1, month1, day1 = nextDay(year1, month1, day1)
         days += 1
     return days
 
+def my_test():
+    assert daysBetweenDates(2013, 1, 1, 2013, 1, 1) == 0
+    assert daysBetweenDates(2013, 1, 1, 2013, 1, 2) == 1
+    assert nextDay(2013, 1, 1) == (2013, 1, 2)
+    assert nextDay(2013, 4, 30) == (2013, 5, 1)
+    assert nextDay(2012, 12, 31) == (2013, 1, 1)
+    assert nextDay(2013, 2, 28) == (2013, 3, 1)
+    assert nextDay(2013, 9, 30) == (2013, 10, 1)
+    assert nextDay(2012, 2, 28) == (2012, 2, 29)
+    assert daysBetweenDates(2012, 1, 1, 2013, 1, 1) == 366
+    assert daysBetweenDates(2013, 1, 1, 2014, 1, 1) == 365
+    assert daysBetweenDates(2013, 1, 24, 2013, 6, 29) == 156
+    print "test finished"
+my_test()
+
 def test():
-    test_cases = [((2012,9,30,2012,10,30),30),
-                  ((2012,1,1,2013,1,1),360),
-                  ((2012,9,1,2012,9,4),3)]
+    test_cases = [((2012,1,1,2012,2,28), 58),
+                  ((2012,1,1,2012,3,1), 60),
+                  ((2011,6,30,2012,6,30), 366),
+                  ((2011,1,1,2012,8,8), 585 ),
+                  ((1900,1,1,1999,12,31), 36523)]
+
     for (args, answer) in test_cases:
         result = daysBetweenDates(*args)
         if result != answer:
