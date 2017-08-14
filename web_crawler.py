12e7c79b4c2cee57056d8d9d0e0732a299632930
Liz Parody <lizparody@Lizs-MacBook-Air.local>
2017-08-14 16:03:56 -0500

    Added the algorithm to rank pages

--- web_crawler.py
+++ web_crawler.py
@@ -97,3 +97,23 @@ def lookup(index, keyword):
     if keyword in index:
         return index[keyword]
     return None
+
+def compute_ranks(graph): # for ranking the webpages
+    d = 0.8 # damping factor
+    numloops = 10
+
+    ranks = {}
+    npages = len(graph)
+    for page in graph:
+        ranks[page] = 1.0 / npages
+
+    for i in range(0, numloops):
+        newranks = {}
+        for page in graph:
+            newrank = (1 - d) / npages
+            for node in graph:
+                if page in graph[node]:
+                    newrank = newrank + d * (ranks[node] / len(graph[node]))
+            newranks[page] = newrank
+        ranks = newranks
+    return ranks
